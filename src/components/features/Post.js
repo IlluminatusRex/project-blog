989cae05faf781fa2b6df42ce85334c183bfd607
Jakub Czesak <jakub.czesak@cbre.com>
2023-04-23 22:45:44 +0200

    Init and add articles

---
+++ src/components/features/Post.js
@@ -0,0 +1,31 @@
+import { Card, Button, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+//import { useParams } from "react-router-dom";
+//import { useSelector } from "react-redux";
+//import { getPostById } from "../../redux/postsRedux";
+
+const Post = props => {
+
+  //const { postId } = useParams();
+  //const postData = useSelector(state => getPostById(state, postId));
+
+  return (
+    <Col className="py-4 col-4 col-md-4">
+      <Card>
+        <Card.Body >
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            <p><b>Author: </b>{props.author}
+            <br/><b>Published: </b>{props.publishedDate}</p>
+            <p>{props.shortDescription}</p>
+          </Card.Text>
+          <Link key={props.id} to={"/post/" + props.id}>
+            <Button variant="primary">Read more</Button>
+          </Link> 
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default Post;
---
+++ src/components/pages/SinglePost.js
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getPostById } from "../../redux/postRedux";
+
+const SinglePost = () => {
+
+  const { id } = useParams();
+  const postData = useSelector(state => getPostById(state, id));
+
+    return (
+      <div>
+        <h2>{postData.title}</h2>
+        <p><b>Author: </b>{postData.author}
+        <br/><b>Published: </b>{postData.publishedDate}</p>
+        <p>{postData.shortDescription}</p>
+      </div>
+    );
+  };
+
+  export default SinglePost;
